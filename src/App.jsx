9cf2d3394d56fd4d72ed27d2e5d3c635a5c45229
elsayed23 <elsayedkewan123@gmail.com>
2024-04-30 02:51:56 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Layout from './components/layout/Layout';
 import Home from './routes/Home'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom';
 
 
 const App = () => {
 
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: '', element: <Layout />, children: [
         { path: '/', element: <Home /> },
